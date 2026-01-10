26c58e10501ceca8a62130f2af3f0e30a035a15d
rafawang2 <rafawcy@gmail.com>
2026-01-10 22:10:35 +0800

    insert_fixup method -Complete the insertion function of the red-black tree.

--- RedBlack_tree.h
+++ RedBlack_tree.h
@@ -56,6 +56,9 @@ class RB_tree {
     RBnode* NIL;
     RB_tree() {
         NIL = new RBnode(nil, BLACK);
+        NIL->Lchild = NIL;
+        NIL->Rchild = NIL;
+        NIL->parent = nullptr;
         root = NIL;
     }
 
@@ -79,9 +82,11 @@ class RB_tree {
         }
         else if (x == x->parent->Lchild) {  // x在左
             x->parent->Lchild = y;
+            y->parent = x->parent;
         }
         else {
             x->parent->Rchild = y;
+            y->parent = x->parent;
         }
         y->Lchild = x;
         x->parent = y;
@@ -90,7 +95,6 @@ class RB_tree {
     void right_rotate(RBnode* x) {
         RBnode* y = x->Lchild;
         RBnode* T2 = y->Rchild;
-
         // rotate
         x->Lchild = T2;
         if (T2 != NIL) {
@@ -101,12 +105,86 @@ class RB_tree {
         }
         else if (x == x->parent->Lchild) {  // x在左
             x->parent->Lchild = y;
+            y->parent = x->parent;
         }
         else {
             x->parent->Rchild = y;
+            y->parent = x->parent;
         }
         y->Rchild = x;
         x->parent = y;
+    }
+
+    void insert_fixup(RBnode* cur) {
+        cout <<"current: "<<cur->value << " , "<<cur->color<<endl;
+        if (cur->parent!=nullptr)
+            cout <<"parent: "<<cur->parent->value << " , "<<cur->parent->color<<endl;
+        if (cur->parent == nullptr) {   // cur is root
+            if (cur->color == RED) cur->color = BLACK;
+            return;
+        }
+
+        if (cur->color == RED && cur->parent->color == RED) {
+            RBnode* gp = cur->parent->parent;
+            // case 1: both parent & uncle are RED -> change color
+            if (gp != nullptr && gp->Lchild->color == RED && gp->Rchild->color == RED) {
+                printTree();
+                cout << "both parent & uncle are RED -> change color\n";
+                gp->color = RED;
+                gp->Lchild->color = BLACK;
+                gp->Rchild->color = BLACK;
+                cout << "fuck\n";
+                insert_fixup(gp);
+            }
+            else if (cur == cur->parent->Lchild) {   // cur is at left
+                if (cur->parent == gp->Lchild) {
+                    printTree();
+                    cout <<"LL: single right rotate\n";
+                    gp->color = RED;
+                    cur->color = RED;
+                    cur->parent->color = BLACK;
+                    right_rotate(gp);
+                    printTree();
+                }
+                else if (cur->parent == gp->Rchild) {
+                    printTree();
+                    cout <<"RL: right rotate on node "<<cur->parent->value<<endl;
+                    right_rotate(cur->parent);
+                    printTree();
+                    cout <<"then change color & left rotate on node "<<gp->value<<endl;
+                    gp->color = RED;
+                    cur->color = BLACK;
+                    cur->parent->color = RED;
+                    left_rotate(gp);
+                    printTree();
+                }
+            }
+            else if (cur == cur->parent->Rchild) {  // cur is at right
+                if (cur->parent == gp->Lchild) {
+                    printTree();
+                    cout <<"LR: left rotate on node "<<cur->parent->value<<endl;
+                    left_rotate(cur->parent);
+                    printTree();
+                    cout <<"then change color & right rotate on node "<<gp->value<<endl;
+                    gp->color = RED;
+                    cur->color = BLACK;
+                    cur->parent->color = RED;
+                    right_rotate(gp);
+                    printTree();
+                }
+                else if (cur->parent == gp->Rchild) {
+                    printTree();
+                    cout <<"RR: single left rotate\n";
+                    gp->color = RED;
+                    cur->color = RED;
+                    cur->parent->color = BLACK;
+                    cout << gp->value <<endl;
+                    left_rotate(gp);
+                    printTree();
+                }
+            }
+        }
+
     }
 
     void insert(RBnode* cur, int v) {
@@ -138,5 +216,6 @@ class RB_tree {
         else
             parent->Rchild = insertNode;
 
+        insert_fixup(insertNode);
     }
 };
